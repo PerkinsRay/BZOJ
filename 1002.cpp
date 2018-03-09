5b612efbb6e82537fcd63f9d8cb3b7067ba13a65
PerkinsRay <pickon.leo@gmail.com>
2018-03-09 17:41:06 +0800

    Update and rename BigInteger.cpp to 1002.cpp

--- 1002.cpp
+++ 1002.cpp
@@ -203,3 +203,19 @@ ostream & operator<<(ostream &out, const BigInterger &obj)
 
 	return out;
 }
+int main() {
+	__int64 input;
+	cin >> input;
+	vector<BigInterger> arr(input+1>3? input + 1:3);
+	arr[0] = 1;
+	arr[1] = 1;
+	arr[2] = 3;
+	for (int i = 3; i <= input; i++) {
+		arr[i] = arr[i - 1] * 3 - arr[i - 2];
+	}
+	BigInterger Res = 0;
+	for (__int64 i = 0; i <= input; i++)
+		Res += (arr[input - i] * (i*i));
+	cout << Res;
+	return 0;
+}
